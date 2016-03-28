5cc8ff037c45d9623aa3f94dc2dd9a32fe80908a
Mathias Johansson <mathias9807@gmail.com>
2016-03-28 13:42:14 +0200

    Disabled error messages in PHP code

--- index.php
+++ index.php
@@ -19,9 +19,9 @@
 	</div>
 
 	<?php
-		// Enable error checking
-		// error_reporting(E_ALL);
-		// ini_set('display_errors', 1);
+		// Error checking
+		error_reporting(0);
+		ini_set('display_errors', 0);
 
 		session_start();
 
